013e7991ddbd2ea5b3d820d45ab1ff5e713daec1
EmperorBIE <1286030695@qq.com>
2024-09-26 22:23:54 +0800

    test

--- test/run.sh
+++ test/run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# 设置Python环境（如果需要）
-# source /path/to/your/virtualenv/bin/activate
+#PBS -q ai
+#PBS -N mse_xvector_emb512
+#PBS -l select=1:ncpus=8:ngpus=4:mem=128gb
+#PBS -l walltime=119:59:00
+#PBS -j oe
+#PBS -P 12001458
 
-# 运行训练脚本
-python train.py
-
-# 检查训练是否成功
-if [ $? -eq 0 ]; then
-    echo "Training completed successfully."
-else
-    echo "Training failed."
-fi
+cd $PBS_O_WORKDIR
+module load singularity
+export SINGULARITY_BIND="/home/project:/home/project,/scratch:/scratch,/app:/app"
+singularity run --nv /app/apps/containers/kaldi/kaldi-nvidia-22.04-py3.sif << EOF
+source ~/miniconda3/bin/activate ~/miniconda3/envs/wespeaker
+python ./GPUtest.py
